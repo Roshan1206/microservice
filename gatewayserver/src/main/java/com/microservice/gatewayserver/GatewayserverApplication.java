0a240d465e2278d00a516ba65a48c2931ffdcf39
Roshan Sahu <sahuroshan96@gmail.com>
2025-08-24 15:03:46 +0530

    added global timeout and retry for loans in gateway server

--- gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -39,7 +41,11 @@ public class GatewayserverApplication {
 						.path("/microservice/loans/**")
 						.filters(f -> f
 								.rewritePath("/microservice/loans/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig
+										.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(100), 2, true)))
 						.uri("lb://LOANS"))
 				.build();
 	}
