71da2ccb9b2548b8e9246d50bd93fb90def5a493
Roshan Sahu <sahuroshan96@gmail.com>
2025-08-24 13:04:32 +0530

    created gatewayserver, updated docker and Readme files

---
+++ gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,47 @@
+package com.microservice.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+//	rerouting paths of microservice so if we hit localhost:8072/microservice/accounts/create
+//	it will redirect it to localhost:8080/create from that particular service
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/microservice/accounts/**") //gateway path predicate
+						.filters(f -> f
+								.rewritePath("/microservice/accounts/(?<segment>.*)", "/${segment}")//rerouting path
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config
+										.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
+						.uri("lb://ACCOUNTS")) //telling for which service it should. lb://ACCOUNTS -> loadbalancer:service
+				.route(p -> p
+						.path("/microservice/cards/**")
+						.filters(f -> f
+								.rewritePath("/microservice/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/microservice/loans/**")
+						.filters(f -> f
+								.rewritePath("/microservice/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.build();
+	}
+
+}
